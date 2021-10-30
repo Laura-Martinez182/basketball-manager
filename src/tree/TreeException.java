d3666fa1969971355d576720af5d1bfbda49a054
Diego Hidalgo Lopez <d13g0fhl@gmail.com>
2021-10-30 10:50:53 -0500

    Added TreeException

---
+++ src/tree/TreeException.java
@@ -0,0 +1,13 @@
+package tree;
+
+public class TreeException extends Exception {
+
+    public TreeException() {
+        super();
+    }
+
+    public TreeException(String msg) {
+        super(msg);
+    }
+
+}
